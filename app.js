e8d6281db0124f21d2383289d50571a287d4efd3
regenalgrant <regenal@mac.com>
2016-09-26 20:35:11 -0700

    new js

---
+++ app.js
@@ -0,0 +1,61 @@
+function getRanNum(min, max) {
+  min = math.ceiling(min)
+  max = math.floor(max)
+  return this.randCustomersPerHour.push(math.random() * (max - min)) max + min)
+}
+var hours = ['6am', '7am', '8am', '9am'];
+var firstAndPike = {
+  minCustPerHour: 23
+  maxCustPerHour: 65
+  avgCustPerHour: 63
+  randCustomersPer[hours],
+  totalCookiesPerHour[ ],
+  locationName 'firstAndPike';
+	  calcRandCustomersPerHour: function() {
+    for (var i = 0; i < hours.length; i++) {    this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+};
+var hours = ['6am', '7am', '8am', '9am'];
+var seatac airport = {
+  minCustPerHour: 3
+  maxCustPerHour: 24
+  avgCustPerHour: 1
+  randCustomersPer[hours],
+  totalCookiesPerHour[hours],
+  locationName 'seatac airport';
+	  calcRandCustomersPerHour: function() {
+    for (var i = 0; i < hours.length; i++) {    this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+};
+var hours = ['6am', '7am', '8am', '9am'];
+var seattle center = {
+  minCustPerHour: 11
+  maxCustPerHour: 38
+  avgCustPerHour: 4
+  randCustomersPer[hours],
+  totalCookiesPerHour[ ],
+  locationName 'seattle center';
+	  calcRandCustomersPerHour: function() {
+    for (var i = 0; i < hours.length; i++) {    this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+};
+var hours = ['6am', '7am', '8am', '9am'];
+var capital hill = {
+  minCustPerHour: 20
+  maxCustPerHour: 38
+  avgCustPerHour: 2
+  randCustomersPer[hours],
+  totalCookiesPerHour[ ],
+  locationName 'capital hill';
+	  calcRandCustomersPerHour: function() {
+    for (var i = 0; i < hours.length; i++) {    this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+};
+var hours = ['6am', '7am', '8am', '9am'];
+var alki = {
+  minCustPerHour: 2
+  maxCustPerHour: 16
+  avgCustPerHour: 5
+  randCustomersPer[hours],
+  totalCookiesPerHour[ ],
+  locationName 'alki';
+	  calcRandCustomersPerHour: function() {
+    for (var i = 0; i < hours.length; i++) {
+   this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+};
